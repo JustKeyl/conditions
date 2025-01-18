d45cacb50aacff5646a6311df7692d2ffc1cbb32
JustKeyl <kleewik@gmail.com>
2025-01-18 14:14:41 +0300

    Create Timer.cs

---
+++ Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    float seconds = 0;
+    int minutes = 1;
+
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        print(minutes);
+        print(seconds);
+
+        if (seconds<0)
+        {
+            if (minutes > 0)
+            {
+                minutes -= 1;
+                seconds += 59;
+            }
+            else
+            {
+                int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(sceneIndex);
+            }
+        }
+        
+    }
+}
